c11522174790388b61e32f23627778bd51e0f9b2
Yussyafrida C.R <yussyafrida0299@gmail.com>
2022-05-07 20:39:15 +0700

    Added master

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::apiResource('projects', ProjectController::class)->middleware('auth:api');
+
+Route::get('/', function () {
+    return view('welcome');
+});
